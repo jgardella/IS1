6ad93ec2da5d413544dd886bc6ef71a069c9869f
Jason Gardella <jgardell@stevens.edu>
2015-08-13 14:56:14 -0400

    Uncomment matlab line in extract script.

--- extract.sh
+++ extract.sh
@@ -3,6 +3,6 @@
 if [ $# -lt 10 ]; then
     echo "usage: extract.sh <slicesFolder> <minChanSize> <csvFile> <plyFile> <xDim> <yDim> <zDim> <r> <g> <b>\n"
 else
-    #matlab -nodisplay -r "addpath(genpath('matlab/'));extractChannelsFromSlices('$1', $2, '$3');exit"
+    matlab -nodisplay -r "addpath(genpath('matlab/'));extractChannelsFromSlices('$1', $2, '$3');exit"
     ./skel_csvtoply $3 $4 $5 $6 $7 $8 $9 $10
 fi
